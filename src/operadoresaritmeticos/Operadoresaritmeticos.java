1191e162221778553f43577663561dd61bd74303
Martin Garcia <martingarcia@gmail.com>
2022-07-22 20:06:37 -0600

    Nuevo commit

--- src/operadoresaritmeticos/Operadoresaritmeticos.java
+++ src/operadoresaritmeticos/Operadoresaritmeticos.java
@@ -14,7 +14,8 @@ public class Operadoresaritmeticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        system.out.println("Hola mundo");
+        system.out.println("Hola mundo");
     }
     
 }
