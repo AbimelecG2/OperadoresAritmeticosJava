405eb9e48943e88644df6dbb828804f5386af783
Martin Garcia <martingarcia@gmail.com>
2022-07-25 17:51:35 -0600

    Se agrega la Tarea 1

--- src/operadoresaritmeticos/Operadoresaritmeticos.java
+++ src/operadoresaritmeticos/Operadoresaritmeticos.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package operadoresaritmeticos;
-
+import java.util.Scanner; 
 /**
  *
  * @author USER
@@ -14,8 +14,46 @@ public class Operadoresaritmeticos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        system.out.println("Hola mundo");
-        system.out.println("Hola mundo");
+     Scanner input =new Scanner(System.in);
+        int numero1;
+        int numero2;
+        int resta, suma, division, multiplicacion;
+        int opcion;
+        
+            System.out.println("-------MENU------");
+            System.out.println("1 Suma");
+            System.out.println("2 Resta");
+            System.out.println("3 Multiplicacion");
+            System.out.println("4 Division");
+            System.out.print("INGRESE LA OPCION A ELEGIR: ");
+            opcion=input.nextInt();
+            
+        System.out.println("Ingrese el primer numero");
+        numero1=input.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        numero2=input.nextInt();
+        
+        
+            
+            switch(opcion){
+                case 1:
+                    suma=numero1+numero2;
+                    System.out.println("\nLa respuesta de la suma es "+ suma);
+                    break;
+                case 2:
+                    resta=numero1-numero2;
+                    System.out.println("\nLa resta de la resta es "+ resta);
+                    break;
+                case 3:
+                    multiplicacion=numero1*numero2;
+                    System.out.println("\nLa respuesta de la multiplicacion es "+ multiplicacion);
+                    break;
+                case 4:
+                    division=numero1/numero2;
+                    System.out.println("\nLa respuesta de la division es "+ division);
+                    break;
+            
+            }
+        }
     }
-    
-}
+
